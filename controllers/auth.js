0197813f22afdecd99f82ebfc71369dd0206a9a3
Agustin Aguilera <agustin.aguilera424@gmail.com>
2021-05-08 13:21:35 -0300

    User Registration

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,15 +1,21 @@
 const { response } = require('express');
-const bcryptjs = require('bcryptjs')
-
+const bcryptjs = require('bcryptjs');
+const encrypt = require('../helpers/encryption');
+const User = require('../models/user');
 
 const login = (req,res=response)=>{
     res.json({
         tes:'ting'
     })
 }
-const register = (req,res=response)=>{
+const register = async(req,res=response)=>{
+    const {name, password, email} =req.body;
+    const encryptedPassword = encrypt(password);
+    const user = new User({email, name, password});
+    user.password=encryptedPassword
+    await user.save();
     res.json({
-        test:'Register'
+        user
     })
 }
 
---
+++ models/user.js
@@ -0,0 +1,31 @@
+const { Schema, model } = require('mongoose');
+
+
+const UserSchema = Schema({
+    email: {
+        type: String,
+        required: [true, 'Email must be provided'],
+        unique: true
+    },
+    name: {
+        type: String,
+        required: [true, 'Email must be provided'],
+        unique: true
+    },
+    password: {
+        type: String,
+        required: [true, 'Password must be provided'],
+    },
+    available: {
+        type: Boolean,
+        default: true
+    },
+});
+
+
+UserSchema.methods.toJSON = function() {
+    const { __v,available, ...userData  } = this.toObject();    
+    return userData;
+}
+
+module.exports = model( 'User', UserSchema );
