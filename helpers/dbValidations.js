cfc25dc7599afda5512e5892d7e7d275165908da
Agustin Aguilera <agustin.aguilera424@gmail.com>
2021-05-08 14:23:28 -0300

    DB validations

---
+++ helpers/dbValidations.js
@@ -0,0 +1,12 @@
+const User = require('../models/user')
+
+const emailIsAlreadyExists = async (email="")=>{
+    const emailExist = await User.findOne({email});
+    if(emailExist){
+        throw new Error(`The email ${email} is already in use`)
+    }
+}
+
+module.exports={
+    emailIsAlreadyExists
+}
--- routes/auth.js
+++ routes/auth.js
@@ -2,6 +2,7 @@ const { Router } = require('express');
 const { check } = require('express-validator');
 const { login } = require('../controllers/auth');
 const { register } = require('../controllers/auth');
+const { emailIsAlreadyExists } = require('../helpers/dbValidations');
 const { retrieveErrors } = require('../middlewares/retrieveErrors');
 const router = Router();
 
@@ -18,6 +19,7 @@ router.post('/register',[
     check('email','Email is not valid').isEmail(),
     check('password','Password is not valid').not().isEmpty(),
     check('password','Password is too short').isLength(5),
+    check('email').custom( emailIsAlreadyExists ),
     retrieveErrors
 ],register );
 
